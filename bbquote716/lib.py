bab2aaf8df92459616c247217ea6c78319ea5ffd
Amanda Maurell <amanda.maurell@gmail.com>
2022-01-29 19:22:27 -0300

    first push

--- bbquote716/lib.py
+++ bbquote716/lib.py
@@ -5,7 +5,7 @@ import requests
 def get_quote():
     url = 'https://movie-quote-api.herokuapp.com/v1/quote/'
     response = requests.get(url).json()
-    return f"quote: {response['quote']}, role:{response['role']}"
+    return f"quote: {response['quote']}, role:{response['role']}!"
 
 if __name__ == '__main__':
     print(get_quote())
